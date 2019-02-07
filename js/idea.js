b9c3f7c4f6f545a5ce5c430ded337a54ddeab7df
Jeo D <e8gea6w5z8id@opayq.com>
2019-02-07 10:39:06 -0700

    Add feature for deleting ideas

--- js/idea.js
+++ js/idea.js
@@ -2,11 +2,17 @@ class Idea {
   constructor(title, body, quality, id) {
     this.title = title;
     this.body = body;
-    this.quality = quality || 'swill';
+    this.quality = quality || 0;
     this.id = id || Date.now();
   }
 
   save(ideas) {
     localStorage.setItem('ideas', JSON.stringify(ideas));
+  }
+
+  delete(ideas) {
+    const newIdeas = ideas.filter(idea => idea.id !== this.id);
+    this.save(newIdeas)
+    return newIdeas;
   }
 }
--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-const ideas = [];
+let ideas = [];
+
+window.onload = handleOnLoad;
 
 document.querySelector('.form').addEventListener('submit', handleSubmit);
 document.querySelector('.form--submit').addEventListener('submit', handleSubmit);
-
-window.onload = handleOnLoad;
+document.querySelector('.main').addEventListener('click', handleClick)
 
 function handleOnLoad() {
   if(localStorage.getItem('ideas')) {
@@ -30,14 +31,44 @@ function handleSubmit(event) {
 function prependIdeaCard(idea) {
   const { title, body, quality, id } = idea;
   const ideaCard = document.createElement('article');
+  const qualities = ['swill', 'plausible', 'genius'];
   ideaCard.id = id;
+  ideaCard.classList.add('article')
   ideaCard.innerHTML = `
     <h3>${title}</h3>
     <p>${body}</p>
     <div>
-      Quality: ${quality}
+      Quality: ${qualities[quality]}
+      <button class="button--decrease">Decrease Quality</button>
+      <button class="button--increase">Increase Quality</button>
+      <button class="button--delete">Delete</button>
     </div>
   `;
   document.querySelector('.main').prepend(ideaCard);
 }
 
+function handleClick(event) {
+  const { classList } = event.target;
+  if (classList.contains('button--delete')) {
+    deleteIdea(event.target);
+  } else if (classList.contains('button--increase')) {
+    increaseIdeaQuality(event.target);
+  } else if (classList.contains('button--decrease')) {
+    decreaseIdeaQuality(event.target);
+  }
+}
+
+function deleteIdea(target) {
+  const id = parseInt(target.closest('.article').id);
+  const ideaToDelete = ideas.find(idea => idea.id === id)
+  ideas = ideaToDelete.delete(ideas);
+  target.closest('.article').remove();
+}
+
+function increaseIdeaQuality(target) {
+
+}
+
+function decreaseIdeaQuality(target) {
+
+}
